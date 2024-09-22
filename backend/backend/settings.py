9ef63a0fa524cacde06564282de715f20e3130e3
Jether Omictin <jethomictin@gmail.com>
2024-09-22 08:44:20 +0800

    added backend settings configutarion

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '123456789',
+        'HOST': 'localhost',
+        'PORT': '5432', 
     }
 }
+
 
 
 # Password validation
